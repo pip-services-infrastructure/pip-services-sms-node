ae7a48e912b3703b13f8986b702f9cb8d39d1ca1
Dmitriy Krayniy <krdima92@gmail.com>
2018-08-20 13:34:29 +0300

    added DefaultRpcFactory

--- obj/src/container/SmsProcess.js
+++ obj/src/container/SmsProcess.js
@@ -2,10 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const pip_services_container_node_1 = require("pip-services-container-node");
 const SmsServiceFactory_1 = require("../build/SmsServiceFactory");
+const pip_services_rpc_node_1 = require("pip-services-rpc-node");
 class SmsProcess extends pip_services_container_node_1.ProcessContainer {
     constructor() {
         super("sms", "Sms delivery microservice");
         this._factories.add(new SmsServiceFactory_1.SmsServiceFactory);
+        this._factories.add(new pip_services_rpc_node_1.DefaultRpcFactory);
     }
 }
 exports.SmsProcess = SmsProcess;
--- src/container/SmsProcess.ts
+++ src/container/SmsProcess.ts
@@ -2,13 +2,14 @@ import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
 
 import { SmsServiceFactory } from '../build/SmsServiceFactory';
+import { DefaultRpcFactory } from 'pip-services-rpc-node';
 
 export class SmsProcess extends ProcessContainer {
 
     public constructor() {
         super("sms", "Sms delivery microservice");
         this._factories.add(new SmsServiceFactory);
+        this._factories.add(new DefaultRpcFactory);
     }
-
 
 }
